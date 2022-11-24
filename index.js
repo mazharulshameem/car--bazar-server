6dad94b44a7aa82356273074e04e928dc6cd7cbb
Mazharul Shameem <mazharulis.cs.07@gmail.com>
2022-11-24 21:06:23 +0600

    car bazar server

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.twtll.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+  } finally {
+  }
+}
+
+app.get("/", async (req, res) => {
+  res.send("Car Bazar server is running");
+});
+
+app.listen(port, () => console.log(`Car Bazar running on ${port}`));
