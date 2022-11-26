755c5faf28f0aaf77f8dd0f09d8e5b9e58acd05d
Mazharul Shameem <mazharulis.cs.07@gmail.com>
2022-11-26 15:44:48 +0600

    jwt token added

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
 const { MongoClient, ServerApiVersion, ObjectID } = require("mongodb");
 require("dotenv").config();
 const port = process.env.PORT || 5000;
@@ -60,6 +61,18 @@ async function run() {
       }
       const result = await bookingsCollection.insertOne(booking);
       res.send(result);
+    });
+    app.get("/jwt", async (req, res) => {
+      const email = req.query.email;
+      const query = { email: email };
+      const buyer = await buyersCollection.findOne(query);
+      if (buyer) {
+        const token = jwt.sign({ email }, process.env.ACCESS_TOKEN, {
+          expiresIn: "1h",
+        });
+        return res.send({ accessToken: token });
+      }
+      res.status(403).send({ accessToken: "" });
     });
     app.post("/buyers", async (req, res) => {
       const buyers = req.body;
