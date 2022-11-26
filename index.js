2d2d51639a81e615c5976fe15a19cf40a4ebeb16
Mazharul Shameem <mazharulis.cs.07@gmail.com>
2022-11-26 16:47:57 +0600

    authorization added

--- index.js
+++ index.js
@@ -43,6 +43,7 @@ async function run() {
     });
     app.get("/bookings", async (req, res) => {
       const email = req.query.email;
+      console.log("token", req.headers.authorization);
       const query = { email: email };
       const bookings = await bookingsCollection.find(query).toArray();
       res.send(bookings);
